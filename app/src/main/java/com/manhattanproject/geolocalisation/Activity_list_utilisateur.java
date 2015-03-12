57f43e8fb110b2714311ffa43558ebd2e993866d
lulu <lulumahot@hotmail.fr>
2015-03-12 13:53:08 +0100

    Rectification

--- app/src/main/java/com/manhattanproject/geolocalisation/Activity_list_utilisateur.java
+++ app/src/main/java/com/manhattanproject/geolocalisation/Activity_list_utilisateur.java
@@ -13,42 +13,49 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 
 public class Activity_list_utilisateur extends ActionBarActivity {
     private ExpandableListView expandableList;
     private AdapterListUtilisateur adaptor;
+    private ArrayList<Utilisateur> listeUser=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_utilisateur);
-        //new selectAll().execute(null,null,null);
+        select();
 
     }
 
-    private class selectAll extends AsyncTask<Void,Void,Void>{
-        private ArrayList<Utilisateur> listeUser;
-        @Override
-        protected Void doInBackground(Void... params) {
-            Utilisateur courant = new Utilisateur();
-            courant.recup(getApplicationContext());
-            final String[] p={"selectAllUsers.php","pseudo","4rrrrrr"};
-            Requete r=new Requete();
-            String response=r.LancerRequete(p);
-            try{
-                JSONArray jArray = new JSONArray(response);
-                for(int i=0;i<jArray.length();i++){
-                    JSONObject json_data = jArray.getJSONObject(i);
-                    listeUser.add(i,new Utilisateur(i, json_data.getString("pseudo"), null, null, null));
-                }
-            }catch(JSONException e){
-                Log.e("log_tag", "Error parsing data " + e.toString());
-            }
-            expandableList = (ExpandableListView) findViewById(R.id.expandableListAmi);
-            adaptor = new AdapterListUtilisateur(getApplicationContext(), listeUser);
-            expandableList.setAdapter(adaptor);
-            return null;
+    public Void select(){
+        Utilisateur courant = new Utilisateur();
+        courant.recup(getApplicationContext());
+        final String[] p={"selectAllUsers.php","pseudo","4rrrrrr"};
+        Requete r=new Requete();
+        r.execute(p);
+        try {
+            r.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
+        String response=r.getResult();
+        try{
+            JSONArray jArray = new JSONArray(response);
+            for(int i=0;i<jArray.length();i++){
+                JSONObject json_data = jArray.getJSONObject(i);
+                listeUser.add(i,new Utilisateur(i, json_data.getString("pseudo"), null, null, null));
+            }
+        }catch(JSONException e){
+            Log.e("log_tag", "Error parsing data " + e.toString());
+        }
+        expandableList = (ExpandableListView) findViewById(R.id.expandableListAmi);
+        adaptor = new AdapterListUtilisateur(getApplicationContext(), listeUser);
+        expandableList.setAdapter(adaptor);
+        return null;
     }
+
 }
