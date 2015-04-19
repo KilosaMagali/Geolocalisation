3eba66efeca5f52ca5773d2a0eb9ed84c7c298c9
WeDemBoyz <kmagali120@gmail.com>
2015-04-19 08:12:39 +0200

    Ajout derniere position-profil ami

--- app/src/main/java/com/manhattanproject/geolocalisation/Activity_profil_Utilisateur.java
+++ app/src/main/java/com/manhattanproject/geolocalisation/Activity_profil_Utilisateur.java
@@ -2,13 +2,21 @@ package com.manhattanproject.geolocalisation;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 
 public class Activity_profil_Utilisateur extends Activity {
@@ -18,6 +26,7 @@ public class Activity_profil_Utilisateur extends Activity {
     Ami user;
     EditText statut;
     ToggleButton toggleButton;
+    TextView addressZone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +35,20 @@ public class Activity_profil_Utilisateur extends Activity {
         setContentView(R.layout.activity_profil_utilisateur);
         pseudo = (TextView)findViewById(R.id.pseudo);
         statut = (EditText)findViewById(R.id.Statut);
-        //partagePos = (CheckBox)findViewById(R.id.checkBoxPartagePos);
         imagebtn = (ImageButton)findViewById(R.id.imageButton);
         toggleButton=(ToggleButton)findViewById(R.id.toggleButton);
+        addressZone=(TextView)findViewById(R.id.addressZone);
         if(!user.getPseudo().equals(""))
             pseudo.setText(user.getPseudo());
         if(!user.getStatut().equals(""))
             statut.setText(user.getStatut());
-            //partagePos.setChecked(user.);
         imagebtn.setImageBitmap(user.getImage());
         toggleButton.setChecked(user.isConnect());
-        //partagePos.setChecked(user.getPartagePos());
+        String address= getAddress(user.getPosition());
+        if(address!=null) addressZone.setText(address);
         statut.setEnabled(false);
-        //partagePos.setEnabled(false);
         imagebtn.setEnabled(false);
+        toggleButton.setEnabled(false);
     }
 
    public void retrieveDataPassed() {
@@ -60,8 +69,28 @@ public class Activity_profil_Utilisateur extends Activity {
        user=new Ami(idAmi,positionAmi,pseudoAmi,statut);
        if(bitmapImageAmi!=null)
        user.setImage(bitmapImageAmi);
+       user.setConnect(connect);
+
 
 
    }
+    public String getAddress(LatLng location) {
+        Geocoder geocoder;
+        List<Address> addresses=new ArrayList<>();
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.w("Location", "Can not get Address!");
+        }
+
+        String address = addresses.get(0).getAddressLine(0);
+        String city = addresses.get(0).getLocality();
+        String country = addresses.get(0).getCountryName();
+        return  "Dernière position: "+ address+" , \n"+city+" , "+country;
+
+    }
 
 }
